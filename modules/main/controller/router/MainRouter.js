635003b3b6a36fe9eb44da783db901fbebc58069
Alprensia <tonite32@gmail.com>
2016-01-28 09:42:03 +0900

    Update routing policy

--- modules/main/controller/router/MainRouter.js
+++ modules/main/controller/router/MainRouter.js
@@ -1,8 +1,17 @@
 module.exports = function(app)
 {
-	//You can use regexp at path.
-	app.get('/', function (req, res)
+	app.get('/*', function (req, res)
 	{
+		var split = req.path.split("/");
+		
+		var param = {};
+		for(var i=1; i<split.length; i++)
+		{
+			param[i] = split[i];
+		}
+		
+		param.name = "Alprensia";
+		
 		res.render("index", param);
 	});
 };
