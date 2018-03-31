51b2a18cc9562fcf304596f10e19b857cbe4d185
Rayim <rayim@Rayim-ui-MacBook-Pro.local>
2018-03-31 21:30:28 +0900

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,12 +3,12 @@ module.exports =
     apps :
     [
         {
-            name: "gorza",
+            name: "halloween",
             script: "./server.js",
             env:
             {
                 "PORT": 3000,
-                "MONGODB_URI": "mongodb://gorza-dev:gorza-dev@cluster0-shard-00-00-fbe5x.mongodb.net:27017,cluster0-shard-00-01-fbe5x.mongodb.net:27017,cluster0-shard-00-02-fbe5x.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin",
+                "MONGODB_URI": "",
                 "NODE_ENV": "dev"
             },
             env_production:
